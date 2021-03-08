54945deb0ab263318ce8243f68cc5452fcf59514
Filip Brkić <filipbrkic5@gmail.com>
2021-03-08 23:13:27 +0100

    w

---
+++ MVC2021/Migrations/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC2021.Migrations
+{
+    public class Configuration : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
